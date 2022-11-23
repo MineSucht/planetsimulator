0b9fc480c4b44909eea58363b810383177a48709
Tom <44319932+MineSucht@users.noreply.github.com>
2022-11-23 01:00:27 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -3,6 +3,8 @@ local Window = OrionLib:MakeWindow({Name = "Planet Simulator", HidePremium = fal
 local _selectedPlanet = nil
 local massTarget = game.Players.LocalPlayer.Name.."MassTarget"
 local popups = false
+local rebirth = false
+local rebirthType = "Brown Dwarf"
 local Tab = Window:MakeTab({
 	Name = "Cheats",
 	Icon = "rbxassetid://4483345998",
@@ -64,6 +66,29 @@ Section:AddToggle({
 	end
 })
 
+local rebirth = Tab:AddSection({
+	Name = "Rebirth"
+})
+
+rebirth:AddDropdown({
+	Name = "Select Rebirth Planet",
+	Default = "Brown Dwarf",
+	Options = {"Brown Dwarf","Red Dwarf","Blue Dwarf","Protostar","Star","Red Giant","Blue Giant",
+	"Red Supergiant","Blue Supergiant","Red Hypergiant","Blue Hypergiant","White Dwarf",
+	"Nebula","Solar System","Neutron Star","Pulsar","Black Hole","Supermassive Black Hole",
+	"Small Galaxy","Galaxy","Giant Galaxy","Supergiant Galaxy","Universe","Multiverse","Infinity"
+	},
+	Callback = function(Value)
+		rebirthType = Value
+	end    
+})
+rebirth:AddToggle({
+	Name = "Toggle Auto Rebirth",
+	Default = false,
+	Callback = function()
+		rebirth = not rebirth
+	end
+})
 local Tab2 = Window:MakeTab({
 	Name = "Credits",
 	Icon = "rbxassetid://4483345998",
@@ -88,6 +113,7 @@ Tab2:AddButton({
 task.spawn(function()
     -- Load up Values
     popups = false
+	rebirth = false
 	wait(2)
 	while wait() do
 		-- Auto Popup
@@ -130,6 +156,17 @@ task.spawn(function()
 					OrionLib:MakeNotification({
 					Name = "Teleporter",
 					Content = "No Planet Selected!",
+					Image = "rbxassetid://4483345998",
+					Time = 2
+					})
+			end
+		end
+		if rebirth then
+			if game.Players.LocalPlayer.leaderstats.Rank.Value == rebirthType then
+				game.ReplicatedStorage.SupernovaFunction:InvokeServer()
+				OrionLib:MakeNotification({
+					Name = "Auto Rebirth",
+					Content = "You did a Supernova",
 					Image = "rbxassetid://4483345998",
 					Time = 2
 					})
