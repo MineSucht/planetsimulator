35f4f9feb7303ce8412b8eb9d9572298887ba6ea
Tom <44319932+MineSucht@users.noreply.github.com>
2022-11-25 20:12:45 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -577,7 +577,7 @@ function FreshlyUI:MakeWindow(WindowConfig)
 				Font = Enum.Font.GothamBold,
 				ClipsDescendants = true
 			}), "Text"),
-			AddThemeObject(SetProps(MakeElement("Label", "", 12), {
+			AddThemeObject(SetProps(MakeElement("Label", "User", 12), {
 				Size = UDim2.new(1, -60, 0, 12),
 				Position = UDim2.new(0, 50, 1, -25),
 				Visible = true
